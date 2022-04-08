b2e45c5d92cee605709ddfc7ba3e75e75ef7692b
mkshine65 <mkshine65@gmail.com>
2022-04-08 12:46:28 +0530

    Init

---
+++ src/test/java/com/example/springkakfa/SpringKakfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkakfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKakfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
